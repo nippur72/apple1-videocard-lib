13f3ed6384f614098360297ed7d263a054f7014e
nino-porcino <nino.porcino@gmail.com>
2021-12-03 11:15:42 +0100

    Update c.bat

--- c.bat
+++ c.bat
@@ -1,8 +1,13 @@
-@echo ======================== APPLE 1 =================================================
-call kickc -t asm6502 -D=APPLE1 test.c -o test_apple1.prg -e
-copy test.prg test_apple1.prg
+rem call kickc -t asm6502 -D=APPLE1 bug1.c -o bug1.prg -e
+rem goto uscita
+
 @echo ======================== VIC20 ===================================================
 call kickc -t VIC20 -D=VIC20 test.c -o test_vic20.prg -e
 rem call kickc -vasmoptimize -vasmout -vcreate -vfragment -vliverange -vloop -vnonoptimize -voptimize -vparse -vsequence -vsizeinfo -vunroll -vuplift -t VIC20 -D=VIC20 test.c -o test_vic20.prg -e
 copy test.prg test_vic20.prg
 
+@echo ======================== APPLE 1 =================================================
+call kickc -t asm6502 -D=APPLE1 test.c -o test_apple1.prg -e
+copy test.prg test_apple1.prg
+
+:uscita
