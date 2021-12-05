388ed3c50f4840c3f84ac4169152dfd0d2a37795
nino-porcino <nino.porcino@gmail.com>
2021-12-05 16:20:44 +0100

    use 8K configuration

--- c.bat
+++ c.bat
@@ -1,13 +1,9 @@
-rem call kickc -t asm6502 -D=APPLE1 bug1.c -o bug1.prg -e
-rem goto uscita
-
 @echo ======================== VIC20 ===================================================
-call kickc -t VIC20 -D=VIC20 test.c -o test_vic20.prg -e
+call kickc -t VIC20_8K -D=VIC20 test.c -o test_vic20.prg -e
 rem call kickc -vasmoptimize -vasmout -vcreate -vfragment -vliverange -vloop -vnonoptimize -voptimize -vparse -vsequence -vsizeinfo -vunroll -vuplift -t VIC20 -D=VIC20 test.c -o test_vic20.prg -e
 copy test.prg test_vic20.prg
 
 @echo ======================== APPLE 1 =================================================
 call kickc -t asm6502 -D=APPLE1 test.c -o test_apple1.prg -e
 copy test.prg test_apple1.prg
-
-:uscita
+call node hexdump > test_apple1.woz
