5cc3f68db82172480a95e6aee9bb734fabf6bace
nino-porcino <nino.porcino@gmail.com>
2021-12-15 17:37:44 +0100

    fix build script

--- tools/build.bat
+++ tools/build.bat
@@ -3,11 +3,11 @@
 @SET TARGET=%1
 
 @echo ======================== VIC20 ===================================================
-call kickc -t ..\kickc\VIC20_8K -D=VIC20 %TARGET%.c -o out\%TARGET%_vic20.prg -e
+call kickc -targetdir ..\kickc\ -t VIC20_8K -D=VIC20 %TARGET%.c -o out\%TARGET%_vic20.prg -e
 copy out\%TARGET%.prg out\%TARGET%_vic20.prg
 
 @echo ======================== APPLE 1 =================================================
-call kickc -t ..\kickc\apple1 -D=APPLE1 %TARGET%.c -o out\%TARGET%_apple1.prg -e
+call kickc -targetdir ..\kickc\ -t apple1 -D=APPLE1 %TARGET%.c -o out\%TARGET%_apple1.prg -e
 
 @rem builds the apple1 eprom file
 call node ..\tools\mkeprom out out\%TARGET%_apple1.bin
