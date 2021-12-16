3403dea8c97dd3a50c5005d9a7c60001e0bd20e6
nino-porcino <nino.porcino@gmail.com>
2021-12-16 18:35:34 +0100

    rename symbol

--- tetris/draw.h
+++ tetris/draw.h
@@ -37,8 +37,8 @@
 void draw_tile(byte x, byte y, byte ch, byte col) {
    byte *source = &FONTS[(word)ch*8];
    word addr = x*8 + y*256;
-   tms_set_vram_write_addr(SCREEN2_PATTERN_TABLE + addr); for(byte i=0;i<8;i++) { TMS_WRITE_DATA_PORT(source[i]); NOP; /*NOP;*/ /*NOP; NOP;*/ /*NOP; NOP; NOP; NOP;*/ }
-   tms_set_vram_write_addr(SCREEN2_COLOR_TABLE   + addr); for(byte i=0;i<8;i++) { TMS_WRITE_DATA_PORT(col);       NOP; /*NOP;*/ /*NOP; NOP;*/ /*NOP; NOP; NOP; NOP;*/ }
+   tms_set_vram_write_addr(TMS_PATTERN_TABLE + addr); for(byte i=0;i<8;i++) { TMS_WRITE_DATA_PORT(source[i]); NOP; /*NOP;*/ /*NOP; NOP;*/ /*NOP; NOP; NOP; NOP;*/ }
+   tms_set_vram_write_addr(TMS_COLOR_TABLE   + addr); for(byte i=0;i<8;i++) { TMS_WRITE_DATA_PORT(col);       NOP; /*NOP;*/ /*NOP; NOP;*/ /*NOP; NOP; NOP; NOP;*/ }
 }
 
 void draw_string(byte x, byte y, byte *s, byte color) {
