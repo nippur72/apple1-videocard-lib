9d54d91a7fe56603148f3d48b01f9ed778b4d1d6
nino-porcino <nino.porcino@gmail.com>
2021-12-22 18:41:26 +0100

    fix bug in clear_sprites()

--- lib/sprites.h
+++ lib/sprites.h
@@ -11,10 +11,10 @@ void tms_clear_sprites() {
    // set sprite coordinates to (0,0) and set pattern name 0
    tms_set_vram_write_addr(TMS_SPRITE_ATTRS);       
    for(byte i=0;i<32;i++) {
-      TMS_WRITE_DATA_PORT((6+i)*8); NOP; NOP; NOP; NOP; // y coordinate
-      TMS_WRITE_DATA_PORT((6+i)*8); NOP; NOP; NOP; NOP; // x coordinate
-      TMS_WRITE_DATA_PORT(0);       NOP; NOP; NOP; NOP; // name
-      TMS_WRITE_DATA_PORT(i);       NOP; NOP; NOP; NOP; // color
+      TMS_WRITE_DATA_PORT(0); NOP; NOP; NOP; NOP; // y coordinate
+      TMS_WRITE_DATA_PORT(0); NOP; NOP; NOP; NOP; // x coordinate
+      TMS_WRITE_DATA_PORT(0); NOP; NOP; NOP; NOP; // name
+      TMS_WRITE_DATA_PORT(0); NOP; NOP; NOP; NOP; // color
    }
 }
 #endif
