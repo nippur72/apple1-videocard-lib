0e08c15e17524b09aa80774466161a8a387711d0
nino-porcino <nino.porcino@gmail.com>
2022-02-07 18:13:35 +0100

    rename variable

--- tools/build.bat
+++ tools/build.bat
@@ -1,16 +1,16 @@
 @rem === BUILD SCRIPT ===
 
-@SET TARGET=%1
+@SET FNAME=%1
 @SET TMS9918=..\..
 
 @echo ======================== APPLE 1 JUKEBOX =================================================
-call kickc -includedir %TMS9918%\lib -targetdir %TMS9918%\kickc\ -t apple1_jukebox %TARGET%.c -o out\%TARGET%.prg -e
-call node %TMS9918%\tools\mkeprom out out\%TARGET%_jukebox.bin
+call kickc -includedir %TMS9918%\lib -targetdir %TMS9918%\kickc\ -t apple1_jukebox %FNAME%.c -o out\%FNAME%.prg -e
+call node %TMS9918%\tools\mkeprom out out\%FNAME%_jukebox.bin
 
 @echo ======================== APPLE 1 =================================================
-call kickc -includedir %TMS9918%\lib -targetdir %TMS9918%\kickc\ -t apple1 %TARGET%.c -o out\%TARGET%.prg -e -Xassembler="-symbolfile"
-call node %TMS9918%\tools\prg2bin -i out\%TARGET%.prg -o out\%TARGET%.bin
-call node %TMS9918%\tools\prg2woz -i out\%TARGET%.prg -o out\%TARGET%.woz
+call kickc -includedir %TMS9918%\lib -targetdir %TMS9918%\kickc\ -t apple1 %FNAME%.c -o out\%FNAME%.prg -e -Xassembler="-symbolfile"
+call node %TMS9918%\tools\prg2bin -i out\%FNAME%.prg -o out\%FNAME%.bin
+call node %TMS9918%\tools\prg2woz -i out\%FNAME%.prg -o out\%FNAME%.woz
 
 @rem clean up files
 @del out\apple1_codeseg.bin
--- tools/build.sh
+++ tools/build.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-TARGET=$1
+FNAME=$1
 TMS9918=../..
 
 mkdir -p out
 
 echo ======================== APPLE 1 JUKEBOX =================================================
-kickc.sh -includedir ${TMS9918}/lib -targetdir ${TMS9918}/kickc/ -t apple1_jukebox ${TARGET}.c -o out/${TARGET}.prg -e
-node ${TMS9918}/tools/mkeprom out out/${TARGET}_jukebox.bin
+kickc.sh -includedir ${TMS9918}/lib -targetdir ${TMS9918}/kickc/ -t apple1_jukebox ${FNAME}.c -o out/${FNAME}.prg -e
+node ${TMS9918}/tools/mkeprom out out/${FNAME}_jukebox.bin
 
 echo ======================== APPLE 1 =================================================
-kickc.sh -includedir ${TMS9918}/lib -targetdir ${TMS9918}/kickc/ -t apple1 ${TARGET}.c -o out/${TARGET}.prg -e
-node ${TMS9918}/tools/prg2bin -i out/${TARGET}.prg -o out/${TARGET}.bin
-node ${TMS9918}/tools/prg2woz -i out/${TARGET}.prg -o out/${TARGET}.woz
+kickc.sh -includedir ${TMS9918}/lib -targetdir ${TMS9918}/kickc/ -t apple1 ${FNAME}.c -o out/${FNAME}.prg -e
+node ${TMS9918}/tools/prg2bin -i out/${FNAME}.prg -o out/${FNAME}.bin
+node ${TMS9918}/tools/prg2woz -i out/${FNAME}.prg -o out/${FNAME}.woz
 
 # clean up files
 rm -f out/apple1_codeseg.bin
