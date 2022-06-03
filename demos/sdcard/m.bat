132dca24e64c142ebfb062dc673585465018bec4
nino-porcino <nino.porcino@gmail.com>
2022-06-03 09:56:39 +0200

    print entry points for applesoft basic

--- demos/sdcard/m.bat
+++ demos/sdcard/m.bat
@@ -1,2 +1,6 @@
 call ..\..\tools\build sdcard
 copy out\sdcard.prg ..\..\..\apple1-emu\software\sdcard.prg /y
+
+@grep comando_load_bas out/sdcard.sym
+@grep comando_asave out/sdcard.sym
+@grep chksum_table out/sdcard.sym
